4968e68a9fd589f3d9412943c4da97d78d044490
Shristi Singh <129602991+Kool-S@users.noreply.github.com>
2025-08-01 03:14:15 +0530

    Update A. Recycling Centre.cpp

--- contests/2128/A. Recycling Centre.cpp
+++ contests/2128/A. Recycling Centre.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void solve() {
+   int n,c;
+   cin>>n>>c;
+   int cnt=0;
+   map<int,int>mp;
+   for(int i=0; i<n; i++){
+       int x;
+       cin>>x;
+       if(c/x>=1) mp[log2(c/x)]++;
+       else cnt++;
+   }
+   int it=0;
+   for(auto el:mp){
+       if(el.first<it) cnt+=el.second;
+       else{
+           cnt+=max(0,el.second-(el.first+1-it));
+           it+=min(el.second,el.first+1-it);
+           
+       }
+   }
+   cout<<cnt;
+}
+
+int main() {
+    
+    int T;
+   cin>>T;
+   while(T--){
+       solve();
+       cout<<endl;
+       
+   }
+    return 0;
+}
